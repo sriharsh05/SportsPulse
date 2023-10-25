07de67dae62b9fba285fdb33fcde28b1890e8ef6
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-10-25 19:14:03 +0530

    implement your news section in article options

--- src/components/articles/ArticleOptions.tsx
+++ src/components/articles/ArticleOptions.tsx
@@ -8,7 +8,8 @@ import LoadingSpinner from "../LoadingSpinner";
 export default function ArticleOptions() {
   const articleListState: ArticleListState = useArticleListState();
   const [sportsList, setSportsList] = useState<Sport[]>([]);
-  const [selectedSport, setSelectedSport] = useState(0);
+  const [selectedSport, setSelectedSport] = useState(-1);
+  const [yourNews, setYourNews] = useState(true);
 
   useEffect(() => {
     const fetchSports = async () => {
@@ -19,9 +20,7 @@ export default function ArticleOptions() {
   }, []);
 
   if (articleListState.isLoading) {
-    return (
-      <LoadingSpinner/>
-    );
+    return <LoadingSpinner />;
   }
 
   if (articleListState.isError) {
@@ -35,6 +34,18 @@ export default function ArticleOptions() {
   return (
     <div className="w-full">
       <div className="flex space-x-1 p-1">
+        <button
+          className={
+            yourNews
+              ? "flex border border-gray-400 rounded-lg p-2.5 my-3 text-sm font-medium leading-5 bg-teal-400 text-gray-700"
+              : "bg-white-400 border border-teal-400 rounded-lg p-2.5 my-3 text-sm font-medium text-gray-700"
+          }
+          onClick={() => {
+            setSelectedSport(-1), setYourNews(true);
+          }}
+        >
+          Your news
+        </button>
         {sportsList.map((sports, index) => (
           <button
             key={sports.id}
@@ -43,27 +54,39 @@ export default function ArticleOptions() {
                 ? "flex border border-gray-400 rounded-lg p-2.5 my-3 text-sm font-medium leading-5 bg-teal-400 text-gray-700"
                 : "bg-white-400 border border-teal-400 rounded-lg p-2.5 my-3 text-sm font-medium text-gray-700"
             }
-            onClick={() => setSelectedSport(index)}
+            onClick={() => {
+              setSelectedSport(index), setYourNews(false);
+            }}
           >
             {sports.name}
           </button>
         ))}
       </div>
       <div className="mt-2">
-        {sportsList.map((sports, index) => (
-          <div
-            key={sports.id}
-            style={{ display: selectedSport === index ? "block" : "none" }}
-          >
-            <div className="flex flex-col gap-3">
-              {articleListState.articles
-                .filter((article) => article.sport.id === sports.id)
-                .map((article) => (
+        {sportsList.map((sports, index) =>
+          yourNews ? (
+            <div key={sports.id}>
+              <div className="flex flex-col gap-3">
+                {articleListState.articles.map((article) => (
                   <ArticleCard key={article.id} {...article} />
                 ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ) : (
+            <div
+              key={sports.id}
+              style={{ display: selectedSport === index ? "block" : "none" }}
+            >
+              <div className="flex flex-col gap-3">
+                {articleListState.articles
+                  .filter((article) => article.sport.id === sports.id)
+                  .map((article) => (
+                    <ArticleCard key={article.id} {...article} />
+                  ))}
+              </div>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
